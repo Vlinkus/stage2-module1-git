de7df7e09458e714c9ab1a26a1008ac6494886f8
VLinkus <LinkusVyte@gmail.com>
2024-01-09 12:45:12 +0200

    mend

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://https://github.com/Vlinkus/stage2-module1-git-Repo";
+    public static String REPO_LINK = "https://github.com/Vlinkus/stage2-module1-git-Repo";
 }
